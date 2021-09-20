b48f46f21025aaaae9900eebce76becb7f5442af
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-09-20 15:19:48 -0300

    Setup app React

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          What the Fuck this shit to run. It's more easy to curse someone than run this shit
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
